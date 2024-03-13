c59cd8f3ef51dca5121979635d6c9573ea61688e
jbean97 <jbsbin84@gmail.com>
2024-03-13 11:57:02 +0900

    sift down 수정

--- priority_queue.py
+++ priority_queue.py
@@ -40,11 +40,11 @@ class PriorityQueue:
       smallest = index
 
       if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][1] > self.heap[smallest][1]:
+        self.heap[left_child_index][1] < self.heap[smallest][1]:
         smallest = left_child_index
 
       if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][1] > self.heap[smallest][1]:
+        self.heap[right_child_index][1] < self.heap[smallest][1]:
         smallest = right_child_index
 
       if smallest != index:
